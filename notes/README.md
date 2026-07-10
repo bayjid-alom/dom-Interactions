2e182dfe7a18c96153080b13d25bff41df5de698
bayjidalom <bayjidalom1005@gmail.com>
2026-07-10 16:20:12 +0600

    feat: implement button toggle

--- notes/README.md
+++ notes/README.md
@@ -23,7 +23,36 @@ Thrive count : 3
 - <button onclick="toggleStyle('all-filter-btn')" class="bg-black text-white px-5 py-2 rounded-md mr-2" id="all-filter-btn">All</button>
 
  - make ()
-- all button select and eventListener
+- all button select and eventListener or function using too make short the code
+- <button onclick="toggleStyle('struggling-filter-btn')
+- যে বাটনে ক্লিক করা হচ্ছে তার আইডি কে দিয়ে দিচ্ছে।
+- এবার --
+  - zekno ekta btn e click korle 3 ta btn er color yash kore dibo.... er age 3 ta btn er bg-black text-white remove kore dite hobe
+  - zeta select korbo tar bg--- black 
+
+
+
+### why not add bg-black?  first remove bg-gray
+```
+  function toggleStyle(id) {
+    // console.log("clicked", id)
+    allFilterBtn.classList.remove("bg-black", "text-white")
+    thrivingBtn.classList.remove("bg-black", "text-white")
+    strugglingBtn.classList.remove("bg-black", "text-white")
+
+
+    allFilterBtn.classList.add("bg-gray-300", "text-black")
+    thrivingBtn.classList.add("bg-gray-300", "text-black")
+    strugglingBtn.classList.add("bg-gray-300", "text-black")
+
+
+    <!-- Fix process -->
+    const selected = document.getElementById(id);
+    selected.classList.remove("bg-gray-300", "text-black")
+    selected.classList.add("bg-black", "text-white")
+}
+```
+
 
 
 
--- script/script.js
+++ script/script.js
@@ -5,13 +5,18 @@ let total = document.getElementById("total");
 let thrivingCount = document.getElementById("thriving-count");
 let strugglingCount = document.getElementById("struggling-count");
 
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+const thrivingBtn = document.getElementById("thriving-filter-btn");
+const strugglingBtn = document.getElementById("struggling-filter-btn")
+
+
 const allCardSection = document.getElementById("all-cards");
 // console.log(allCardSection.children.length)
 
 const mainContainer = document.querySelector("main");
 console.log(mainContainer)
 
-const allFilterBtn = document.getElementById("all-filter-btn");
 
 function calculateCount() {
     total.innerText = allCardSection.children.length;
@@ -23,5 +28,18 @@ calculateCount()
 
 
 function toggleStyle(id) {
-    console.log("clicked", id)
+    // console.log("clicked", id)
+    allFilterBtn.classList.remove("bg-black", "text-white")
+    thrivingBtn.classList.remove("bg-black", "text-white")
+    strugglingBtn.classList.remove("bg-black", "text-white")
+
+
+    allFilterBtn.classList.add("bg-gray-300", "text-black")
+    thrivingBtn.classList.add("bg-gray-300", "text-black")
+    strugglingBtn.classList.add("bg-gray-300", "text-black")
+
+    const selected = document.getElementById(id);
+    selected.classList.remove("bg-gray-300", "text-black")
+    selected.classList.add("bg-black", "text-white")
+
 }
